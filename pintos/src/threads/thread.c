5402db6da189b278c7c35aae471907378ea11273
Samiya Caur <36728314+samiyac@users.noreply.github.com>
2019-02-09 15:45:35 +0530

    Update thread.c

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -504,11 +504,6 @@ init_thread (struct thread *t, const char *name, int priority)
 
   list_init(&t->waiting_threads);
 
-  if(thread_mlfqs && t!=initial_thread){
-    struct thread *curr=thread_current();
-    t->nice_value=curr->nice_value;
-  }
-
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
   intr_set_level (old_level);
